eceae2488dfdafccd0883444a87f68375de5272e
Kyle-HeatEng <kyle@heat-engineer.com>
2024-02-26 23:00:45 +0000

    Working Snake Movemenent

--- Assets/scripts/Snake/Snake.cs
+++ Assets/scripts/Snake/Snake.cs
@@ -3,102 +3,63 @@ using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
-    public GameObject SnakeNode;
+    public GameObject snakeNodeGameObject;
+    private List<GameObject> snakeNodeGameObjects;
+
+    public List<SnakeNode> snakeNodes;
     public int SnakeLength = 1;
-    private List<GameObject> SnakeNodes;
     // Start is called before the first frame update
     void Start()
     {
-        SnakeNodes = CreateSnakeNodes(SnakeLength);
+        snakeNodeGameObjects = CreateSnakeNodes(SnakeLength);
+        snakeNodes = snakeNodeGameObjects.ConvertAll(snakeNodeGameObject => snakeNodeGameObject.GetComponent<SnakeNode>());
     }
 
     void OnEnable()
     {
-        EventBridge.OnGameUpdate += Move;
-        EventBridge.OnUpdateSnakeHeadPosition += UpdateNodesDirection;
+        EventBridge.OnGameUpdate += Move;    
     }
 
     void OnDisable()
     {
         EventBridge.OnGameUpdate -= Move;
-        EventBridge.OnUpdateSnakeHeadPosition -= UpdateNodesDirection;
     }
-
 
     public void Move(GameState currentState, GameState updatedState)
     {
-        if (SnakeNodes == null) return;
+        if (snakeNodeGameObjects == null) return;
 
 
-        for (int i = 0; i < SnakeNodes.Count; i++)
+        for (int i = snakeNodeGameObjects.Count - 1; i >= 0; i--)
         {
-            GameObject snakeNodeGameObject = SnakeNodes[i];
             bool isHead = i == 0;
-            SnakeNode snakeNode = snakeNodeGameObject.GetComponent<SnakeNode>();
+
+            SnakeNode snakeNode = snakeNodes[i];
+            SnakeNode parentNode = isHead ? null : snakeNodes[i - 1];
             
-            UpdateNodesPosition(snakeNode, isHead && !CompareVector3.Compare(updatedState.SnakeHeadDirection, snakeNode.NodeDirection));
+            UpdateNodesPosition(snakeNode, parentNode, isHead, updatedState);
             
-            //If the current node instance is the head and the direction has changed
-            if (isHead && !CompareVector3.Compare(updatedState.SnakeHeadDirection, snakeNode.NodeDirection))
-            {
-                var currentNodeDirection = snakeNode.NodeDirection;
-                snakeNode.NodeDirection = updatedState.SnakeHeadDirection;
-
-                Logger.Log("There was a change in direction", LogLocation.Snake, "yellow");
-                Logger.Log("CurrentNodeDirection: " + currentNodeDirection, LogLocation.Snake);
-                Logger.Log("UpdatedState.SnakeHeadDirection: " + updatedState.SnakeHeadDirection, LogLocation.Snake);
-                continue;
-            }
-
-            Vector3 snakeNodeCurrentDirection = snakeNode.NodeDirection;
-            Vector3 prevSnakeNodeDirection = isHead ?  SnakeNodes[i].GetComponent<SnakeNode>().NodeDirection : SnakeNodes[i - 1].GetComponent<SnakeNode>().NodeDirection;
-            Logger.Log("Movement", LogLocation.Snake, "yellow");
-            Logger.Log("PrevSnakeNodeDirection: " + prevSnakeNodeDirection, LogLocation.Snake);
-            Logger.Log("SnakeNodeCurrentDirection: " + snakeNodeCurrentDirection, LogLocation.Snake);
-
+            Vector3 prevSnakeNodeDirection = isHead ? updatedState.SnakeHeadDirection : parentNode.NodeDirection;
 
             snakeNode.NodeDirection = prevSnakeNodeDirection;
 
         }
     }
 
-    private void UpdateNodesPosition(SnakeNode snakeNode, bool isHeadWithUpdatedDirection = false)
+    private void UpdateNodesPosition(SnakeNode snakeNode, SnakeNode parentNode, bool isHead, GameState updatedState)
     {
-        if(isHeadWithUpdatedDirection)
-        {
-            //TODO: need to remove the current direction and place in a different location
-            /**
-            *
-            * |   |   |   |    |   |   |   |
-            * |   | f | s |    |   | s |   |
-            * |   |   |   |    |   | f |   |
-            * |   |   |   |    |   |   |   |
-            *
-            * f = first;
-            * s = second;
-            *
-            */ 
-            Logger.Log("Head with updated direction", LogLocation.Snake, "yellow");
-            
+        if(isHead)
+        {   
             Vector3 headNodeCurrentPosition = snakeNode.transform.position;
             
-            Vector3 headNodeUpdatedPosition = headNodeCurrentPosition + snakeNode.NodeDirection;
+            Vector3 headNodeUpdatedPosition = headNodeCurrentPosition + updatedState.SnakeHeadDirection;
             
             snakeNode.transform.position = headNodeUpdatedPosition;
             
             return;
         }
 
-        Vector3 snakeNodeCurrentPosition = snakeNode.transform.position;
-        
-        Vector3 snakeNodeUpdatedPosition = snakeNodeCurrentPosition + snakeNode.NodeDirection;
-
-        Logger.Log("Updating Node Position", LogLocation.Snake, "yellow");
-        Logger.Log("SnakeNodeUpdatedPosition: " + snakeNodeUpdatedPosition, LogLocation.Snake);
-        Logger.Log("SnakeNodeCurrentPosition: " + snakeNodeCurrentPosition, LogLocation.Snake);
-        Logger.Log("SnakeNodeDirection: " + snakeNode.NodeDirection, LogLocation.Snake);
-
-        snakeNode.transform.position = snakeNodeUpdatedPosition;
+        snakeNode.transform.position = parentNode.transform.position;
     }
 
     private List<GameObject> CreateSnakeNodes(int length)
@@ -107,7 +68,7 @@ public class Snake : MonoBehaviour
 
         for (int i = 0; i < length; i++)
         {
-            var node = Instantiate(SnakeNode, new Vector3(i, 0, 0), Quaternion.identity);
+            var node = Instantiate(snakeNodeGameObject, new Vector3(i, 0, 0), Quaternion.identity);
             
             snakeNodes.Add(node);
         }
@@ -115,26 +76,4 @@ public class Snake : MonoBehaviour
         return snakeNodes;
     }
 
-    private void UpdateNodesDirection(Vector3 newHeadDirection)
-    {
-
-        for (int i = SnakeNodes.Count - 1; i >= 0; i--)
-        {
-            bool isHead = i == 0;
-            if (isHead)
-            {
-                SnakeNode headNode = SnakeNodes[i].GetComponent<SnakeNode>();
-
-                headNode.NodeDirection = newHeadDirection;
-                
-                continue;
-            }
-
-            SnakeNode snakeNode = SnakeNodes[i].GetComponent<SnakeNode>();
-
-            snakeNode.NodeDirection = SnakeNodes[i - 1].GetComponent<SnakeNode>().NodeDirection;
-            
-        }
-
-    }
 }
